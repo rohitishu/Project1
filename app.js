08a1873092d5397ed4ebc0481f16065814ee2337
Rohit Kumar <32030787+rohitishu@users.noreply.github.com>
2020-05-17 19:32:51 +0530

    included mongodb
    project is connected with the database

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var express=require("express");
 var app=express();
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true, useUnifiedTopology: true});
 var bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
- var campgrounds=[
-       {name:"salmon greek",image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547940702f7ed4914d_340.jpg"},
-       {name:"granite hill",image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417c2a7bd1974bc5_340.jpg"},
-       {name:"mountain goat's rest",image:"https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e5074417c2a7bd1974bc5_340.jpg"},
-       {name:"salmon greek",image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547940702f7ed4914d_340.jpg"},
-       {name:"granite hill",image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417c2a7bd1974bc5_340.jpg"},
-       {name:"salmon greek",image:"https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547940702f7ed4914d_340.jpg"},
-       {name:"granite hill",image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417c2a7bd1974bc5_340.jpg"},
-       {name:"mountain goat's rest",image:"https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e5074417c2a7bd1974bc5_340.jpg"}
-       ] 
+var campgroundSchema=new mongoose.Schema({
+name:String,
+image:String,
+description:String
+});
+var Campground=mongoose.model("Campground",campgroundSchema);
+/*Campground.create({
+   name:"granite hill",
+   image:"https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c2973d69e44c1_340.jpg",
+   description:"this is a huge granite hill no bathroom no water only beautiful granite" 
+},function(err,campground){
+if(err)
+{
+   console.log(err);
+}
+else
+{
+   console.log("newly created campground");
+   console.log(campground);
+}
+});*/
 app.get("/",function(req,res)
 {
    res.render("landing"); 
@@ -20,20 +33,51 @@ app.get("/",function(req,res)
  
 app.get("/campgrounds",function(req,res)
 {
- 
-       res.render("campgrounds",{campgrounds:campgrounds});
+ Campground.find({},function(err,allCampgrounds){
+     if(err)
+     {
+        console.log(err);
+     }
+     else
+     {
+       res.render("index",{campgrounds:allCampgrounds}); 
+     }
+ });
+      
 });
 app.post("/campgrounds",function(req,res)
 {
    var name=req.body.name;
    var image=req.body.image;
-   var newCampgrounds={name:name , image:image};
-   campgrounds.push(newCampgrounds);
-   res.redirect("/campgrounds");
+   var desc=req.body.description;
+   var newCampgrounds={name:name , image:image,description:desc};
+   Campground.create(newCampgrounds,function(err,newCreated){
+      if(err)
+      {
+         console.log(err);
+      }
+      else
+      {
+         res.redirect("/campgrounds");
+      }
+   });
+  
 });
 app.get("/campgrounds/new",function(req,res){
    res.render("new.ejs"); 
 });
-app.listen(process.env.PORT,process.env.IP,function(){
+app.get("/campgrounds/:id",function(req,res){
+Campground.findById(req.params.id,function(err,foundCampground){
+if(err)
+{
+   console.log(err);
+}
+else
+{
+   res.render("show",{campground:foundCampground});
+}
+});
+});
+app.listen(9000,function(){
    console.log("the yelmp server has started!!"); 
 });
